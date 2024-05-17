716b6508c00cc4d00f65f5fbdb629b9c81b2c56d
reshma <rrkhajasab22@gmail.com>
2024-05-16 21:26:28 -0700

    new file

---
+++ profit.py
@@ -0,0 +1,25 @@
+a=10
+y=110
+z=120
+b=20.0
+c="Welcome to python data structure"
+d=[1,2,3,4,5]
+e={"name":"maruthi","age":18}
+f=("apple","banana","cherry")
+print("integer:",a)
+print("float:",b)
+print("string:",c)
+print("list:",d)
+print("dictonary:",e)
+print("tuple:",f)
+d.append(7)
+print("list:",d)
+q=len(d)
+print(q)
+w=d.index(2)
+print(w)
+for i in e:
+    print(i)
+    print(y>z)
+    z=len(c)
+    print(z)
